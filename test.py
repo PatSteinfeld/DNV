fea81a3cfe5233b093464247a695efad685f6056
PatSteinfeld <110217700+PatSteinfeld@users.noreply.github.com>
2025-01-29 15:24:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -184,7 +184,7 @@ def main():
         
         pivot_df_1["RC_Total_portfolio_old"] = pivot_df_1.get("RC_Man-Days_old_RC available_Secured", 0) + pivot_df_1.get("RC_Man-Days_old_RC available_Unsecured", 0) + pivot_df_1.get("RC_Man-Day_old_RC Not available_NA",0)
         pivot_df_1["RC_Total_portfolio_new"] = pivot_df_1.get("RC_Man-Days_new_RC available_Secured", 0) + pivot_df_1.get("RC_Man-Days_new_RC available_Unsecured", 0) + pivot_df_1.get("RC_Man-Day_new_RC Not available_NA",0)
-        pivot_df_1["RC_Total_portfolio Diff"] = pivot_df_1["RC_Total_portfolios_new"] - pivot_df_1["RC_Total_portfolio_old"]
+        pivot_df_1["RC_Total_portfolio Diff"] = pivot_df_1["RC_Total_portfolio_new"] - pivot_df_1["RC_Total_portfolio_old"]
 
         # Sorting and selecting columns
         pivot_df = pivot_df[[
