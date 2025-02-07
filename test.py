9cf30124058e8aa119944a44e4c0f41e272600d2
PatSteinfeld <110217700+PatSteinfeld@users.noreply.github.com>
2025-02-07 17:46:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -187,7 +187,7 @@ def main():
         pivot_df_1["RC_Total_portfolio_old"] = pivot_df_1.get("RC_Man-Days_old_RC available_Secured", 0) + pivot_df_1.get("RC_Man-Days_old_RC available_Unsecured", 0) + pivot_df_1.get("RC_Man-Days_old_RC Not available_NA",0)
         pivot_df_1["RC_Total_portfolio_new"] = pivot_df_1.get("RC_Man-Days_new_RC available_Secured", 0) + pivot_df_1.get("RC_Man-Days_new_RC available_Unsecured", 0) + pivot_df_1.get("RC_Man-Days_new_RC Not available_NA",0)
         pivot_df_1["RC_Not Available_portfolio_new"] =  pivot_df_1.get("RC_Man-Days_new_RC Not available_NA",0)
-        pivot_df_1["RC_Not Available_portfolio_old"] =pivot_df_1.get("RC_Man-Days_new_RC Not available_NA",0)
+        pivot_df_1["RC_Not Available_portfolio_old"] = pivot_df_1.get("RC_Man-Days_old_RC Not available_NA",0)
         
         pivot_df_1["RC_Total_portfolio Diff"] = pivot_df_1["RC_Total_portfolio_new"] - pivot_df_1["RC_Total_portfolio_old"]
         pivot_df_1["RC_Not Available_portfolio Diff"] = pivot_df_1["RC_Not Available_portfolio_new"] - pivot_df_1["RC_Not Available_portfolio_old"]
